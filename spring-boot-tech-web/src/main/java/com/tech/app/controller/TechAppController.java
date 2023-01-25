5ec88168b4f66cee129b074d1f3095f3fe37364c
Bhushan Bafna <bhushan.bafna.extern@generali.com>
2023-01-25 14:14:08 +0100

    unwanted import removed

--- spring-boot-tech-web/src/main/java/com/tech/app/controller/TechAppController.java
+++ spring-boot-tech-web/src/main/java/com/tech/app/controller/TechAppController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tech.app.so.AppDetails;
